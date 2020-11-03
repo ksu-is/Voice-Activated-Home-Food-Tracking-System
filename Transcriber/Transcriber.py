f301c26f11799576e0f6d3a05cf7ecb7fa953757
bwasden <69821360+bwasden@users.noreply.github.com>
2020-11-03 15:59:15 -0500

    Create Transcriber.py

---
+++ Transcriber/Transcriber.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr     
+ 
+r = sr.Recognizer()                 
+with sr.Microphone() as source:     
+    print("Speak Anything :")
+    audio = r.listen_in_background(source)        
+    try:
+        text = r.recognize_google(audio)    
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize your voice")
+
