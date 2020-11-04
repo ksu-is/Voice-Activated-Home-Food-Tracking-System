c57a2d7da9e44121e89a081932ad074a31bddb3c
bwasden <69821360+bwasden@users.noreply.github.com>
2020-11-04 13:30:27 -0500

    Update Transcriber.py
    Updated audio recognizer + while loop

--- Transcriber/Transcriber.py
+++ Transcriber/Transcriber.py
@@ -1,11 +1,15 @@
 import speech_recognition as sr     
  
-r = sr.Recognizer()                 
-with sr.Microphone() as source:     
-    print("Speak Anything :")
-    audio = r.listen(source)        
+r = sr.Recognizer()
+text = "default"
+while(not "stop" in text.lower().strip()):            
+    with sr.Microphone() as source:     
+        print("Speak Anything :")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source) 
     try:
         text = r.recognize_google(audio)    
         print("You said : {}".format(text))
     except:
         print("Sorry could not recognize your voice")
+print("exiting")
