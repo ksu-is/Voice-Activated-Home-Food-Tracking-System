06d1b6190be463c5b2da93fcea8597a573d4808c
bwasden <69821360+bwasden@users.noreply.github.com>
2020-11-09 17:23:04 -0500

    def Main():
    Moved main program for easier flow.

--- Inventory_Voice_Assistant.py
+++ Inventory_Voice_Assistant.py
@@ -61,22 +61,23 @@ def respond(msg):
     engine.say(msg)
     engine.runAndWait()
 
-r = sr.Recognizer()
-text = "default"
-while(not "stop" in text.lower().strip()):            
-    with sr.Microphone() as source:     
-        respond("Speak Anything :")
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source) 
-    try:
-        text = r.recognize_google(audio)    
-        print("You said : {}".format(text))
-    except:
-        print("Sorry could not recognize your voice")
-    if text.lower().strip() =='add item':
-        addItem('milk',1)
-    if text.lower().strip() =='remove item':
-        removeItem('milk',1)
+def main():
+    r = sr.Recognizer()
+    text = "default"
+    while(not "stop" in text.lower().strip()):            
+        with sr.Microphone() as source:     
+            respond("Speak Anything :")
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source) 
+        try:
+            text = r.recognize_google(audio)    
+            print("You said : {}".format(text))
+        except:
+            print("Sorry could not recognize your voice")
+        if text.lower().strip() =='add item':
+            addItem('milk',1)
+        if text.lower().strip() =='remove item':
+            removeItem('milk',1)
     
 
-print("exiting")
+    print("exiting")
