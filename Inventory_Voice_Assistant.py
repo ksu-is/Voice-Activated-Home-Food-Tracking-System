0beb6b551890619e63c3bc3d0f6ea73ce022f418
bwasden <69821360+bwasden@users.noreply.github.com>
2020-11-12 12:41:44 -0500

    added comments to explain in detail of how individual functions operate. added type hinting

--- Inventory_Voice_Assistant.py
+++ Inventory_Voice_Assistant.py
@@ -6,7 +6,9 @@ import pyttsx3
 
 sysName='Kitchen'
 
-def respond(msg, speaker):
+#Function allows for program to speak to user#
+#Prints text to console#
+def respond(msg:str, speaker:str):
     engine=pyttsx3.init('sapi5')
     voices=engine.getProperty('voices')
     
@@ -15,38 +17,45 @@ def respond(msg, speaker):
     engine.say(msg)
     engine.runAndWait()
 
-def addItem(itemName,qt=1):
+#Adds item to inventory.csv#
+def addItem(itemName:str,qt:str="1"):
+    #Reading inventory#
     inventoryFile = open('inventory.csv','r+')
     inventoryFile.seek(0)
     lines = inventoryFile.readlines()
     inventoryFile.close()
-    #print(lines)
+    
+    #check to see if item is already inventory#
     found = False
     for index in range(len(lines)):
+        #if item is found in inventory, modifies existing quatity#
         if itemName in lines[index]:
-            #print(lines[index])
             temp = lines[index].split(',')
             temp2= temp[1]
             oldQ=int(temp2)
             lines[index]=itemName+','+str(int(qt+oldQ))+'\n'
             found = True
             break
+    #if item is found in inventory, add new entry#    
     if(not found):
         lines.append(itemName+','+str(qt)+'\n')
+    #overwrite old inventory with new changes#    
     inventoryFile = open('inventory.csv','w+')
     inventoryFile.seek(0) 
     inventoryFile.write("".join(lines))
 
-def removeItem(itemName, qt=1):
+#removes item from inventory.csv#
+def removeItem(itemName:str,qt:str="1"):
     inventoryFile = open('inventory.csv','r+')
     inventoryFile.seek(0)
     lines = inventoryFile.readlines()
     inventoryFile.close()
     #print(lines)
     found = False
+    #check to see if item is already inventory#
     for index in range(len(lines)):
+        #if item is found in inventory, modifies existing quatity#
         if itemName in lines[index]:
-            #print(lines[index])
             temp = lines[index].split(',')
             temp2= temp[1]
             oldQ= int(temp2)
@@ -55,21 +64,24 @@ def removeItem(itemName, qt=1):
                 lines[index]=itemName+','+str(oldQ-int(qt))+'\n'
             else:
                 lines.remove(index)
-            #found = True
             break
     if(not found):
         respond(itemName, ' not found')
+    #overwrite old inventory with new changes#
     inventoryFile = open('inventory.csv','w+')
     inventoryFile.seek(0) 
     inventoryFile.write("".join(lines))
     respond('Removing Item',sysName)    
 
+#listens to speech from user and transcribes to text#
+#If silent, system is listening with no output to user#
 def getAudio(silent=False):
+    #Record user#
     r = sr.Recognizer()
     with sr.Microphone() as source:     
-        #respond("Speak Anything :")
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source) 
+    #transcription of audio#
     try:
         text = r.recognize_google(audio)
         text = text.lower().strip()
@@ -79,9 +91,12 @@ def getAudio(silent=False):
     except:
         if not silent:
             respond("Sorry could not recognize your voice",sysName)
+        #Since speech recognition fails try again#
         return(getAudio(silent))
-    
-def confirm(msg):
+
+#confirms action with user#
+#returns "yes", "no", "nevermind"#    
+def confirm(msg:str)-> str: 
     respond(msg,sysName)
     c = getAudio()
     if 'yes' in c or 'ya' in c or 'yeah' in c:
@@ -94,9 +109,9 @@ def confirm(msg):
         respond('Sorry, I did not quite catch that. Please respond with yes or no.',sysName)
         return(confirm(msg))
 
-#Function isolates the quanitity from the given text and returns quantity
-#Quantity must be stated before the item or Q will return ""
-def findQ(txt):
+#Function isolates the quanitity from the given text and returns quantity#
+#Quantity must be stated before the item or Q will return ""#
+def findQ(txt:str)-> str:
     quantity = ""
     for char in txt:
         if char.isdigit():
@@ -105,23 +120,24 @@ def findQ(txt):
             break
     return quantity
 
-#Function strips out digits and returns the item to be added by the program
-def findItem(txt):
+#Function strips out digits and returns the item to be added by the program#
+def findItem(txt:str)->str:
     item = ""
     item=txt.strip('0123456789').strip()
     return item
 
 def main():
-    # If inventory.csv does not exist, create the file
+    # If inventory.csv does not exist, create the file#
     if (not path.exists("inventory.csv")):
         inventoryFile = open('inventory.csv','w')
         inventoryFile.close()
 
     text = "default"
-    while not "exit kitchen" in text:            
-        print('not shut down kitchen in text','shut down kitchen' in text)
+    #Loop keeps listening until user speaks sysName and will only exit with "exit" + sysName#
+    while not "exit " + sysName in text:            
+        #system will remain silent until user speaks "hey" + sysName#
         text=getAudio(True)
-        if 'hey kitchen' in text:
+        if "hey " + sysName in text:
             while True:
                 respond('What would you like to do?',sysName)
                 text=getAudio()
@@ -129,10 +145,15 @@ def main():
                     respond('ok',sysName)
                     break
 
+                
                 elif text.startswith('add'):
+                    #remove "add" from command so as not to be processed in "addItem" function#
                     text = text[3::].strip()
+                    #extracting quantity from user speech#
                     quantity = findQ(text)
+                    #extracting item from user speech#
                     item = findItem(text)
+                    #if user does not supply item, system prompts user to do so#
                     if item == "":
                         respond('What would you like to add?',sysName)
                         text = getAudio()
@@ -140,6 +161,7 @@ def main():
                         item = findItem(text)
                         if 'nevermind' in item:
                             break
+                    #confirm that user wants to perform action#
                     confirmation = confirm('Just to be sure, you would like to add'+' '+quantity+' '+item)
                     if confirmation == 'nevermind':
                         break
@@ -155,6 +177,7 @@ def main():
                             break
                     if 'nevermind' in item or 'nevermind' in confirmation:
                         break
+                    #adds to inventory.csv#
                     respond('Adding '+quantity +' '+item,sysName)
                     if quantity == "":
                         addItem(item)
@@ -163,9 +186,13 @@ def main():
                     break  
                 
                 elif text.startswith('remove'):
+                    #remove "remove" from command so as not to be processed in "remove" function#
                     item = text[6::].strip()
+                    #extracting quantity from user speech#
                     quantity = findQ(text)
+                    #extracting item from user speech#
                     item = findItem(text)
+                    #if user does not supply item, system prompts user to do so#
                     if item == "":
                         respond('What would you like to remove?',sysName)
                         text = getAudio()
@@ -173,6 +200,7 @@ def main():
                         item = findItem(text)
                         if 'nevermind' in item:
                             break
+                    #confirm that user wants to perform action#    
                     confirmation = confirm('Just to be sure, you would like to remove'+' '+quantity+' '+item)
                     if confirmation == 'nevermind':
                         break
@@ -189,6 +217,7 @@ def main():
                     if 'nevermind' in item or 'nevermind' in confirmation:
                         break
                     respond('Removing '+quantity +' '+item,sysName)
+                    #removes item from inventory.csv#
                     if quantity == "":
                         removeItem(item)
                     else:
